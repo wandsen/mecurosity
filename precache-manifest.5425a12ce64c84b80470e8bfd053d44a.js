7e667a1f84f025e221614bee2a5419626d25d0df
wandsen <dsenwan@gmail.com>
2018-11-27 14:15:03 +0800

    Updates

---
+++ precache-manifest.5425a12ce64c84b80470e8bfd053d44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "670a1ab1201469e589f8",
+    "url": "/mecuriosity/static/css/main.3738789a.chunk.css"
+  },
+  {
+    "revision": "670a1ab1201469e589f8",
+    "url": "/mecuriosity/static/js/main.670a1ab1.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/mecuriosity/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "4f7a5444544367023324",
+    "url": "/mecuriosity/static/js/runtime~main.4f7a5444.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mecuriosity/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed3d64d527bbc5521bc6c8c950d799e9",
+    "url": "/mecuriosity/index.html"
+  }
+];
